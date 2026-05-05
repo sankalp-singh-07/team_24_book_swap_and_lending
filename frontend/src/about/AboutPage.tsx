e5482fbc0e5ca9016fdc9ade2444b7ed1d8f0be0
SARA1326 <saransh1326.fzd@gmail.com>
2026-05-05 15:34:05 +0530

    about page

---
+++ frontend/src/about/AboutPage.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import About from '../components/About';
+import Footer from "../components/Footer";
+
+function AboutPage() {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen">
+        <About />
+      </div>      
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
